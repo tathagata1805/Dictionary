fdb96ad5604e5da7f560f01796ac3f7dc2d0debb
Tathagata Chakraborty <tathagata1805@gmail.com>
2023-04-21 17:48:42 +0530

    final commit
    final commit post development

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by Tathagata Chakraborty
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
